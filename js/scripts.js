617e740a32ed7fe9c10e24c4492bcf1a8086fd0b
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-30 15:00:23 -0700

    display results with correct parameters in div

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,19 @@ $(document).ready(function() {
     var shoppingList = [$("input#item1").val(), $("input#item2").val(), $("input#item3").val(), $("input#item4").val(), $("input#item5").val()]
     console.log(shoppingList);
 
+    var sortedShoppingList = shoppingList.sort();
+    console.log(sortedShoppingList);
+    
+    var shoutedShoppingList = sortedShoppingList.map(function(item) {
+      return item.toUpperCase();
+    }); 
+    console.log(shoutedShoppingList);
+
+    shoutedShoppingList.forEach(function(item) {
+      $(".item").append("<li>" + item + "</li>")
+    })
+
+
     $("#input").hide();
     $("#output").show();
 
