7bd3a95a88e8bcdb6fcd78ac1ee2985dfe6455ee
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-30 14:40:57 -0700

    add input form and convert to jq array

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("#shoppingList").submit(function(event) {
+
+    var shoppingList = [$("input#item1").val(), $("input#item2").val(), $("input#item3").val(), $("input#item4").val(), $("input#item5").val()]
+    console.log(shoppingList);
+
+    event.preventDefault();
+  });
+});
